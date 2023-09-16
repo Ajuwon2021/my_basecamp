e57c0401f4da8bfb58156952972313edaea117b5
EC2 Default User <ec2-user@ip-172-31-72-174.ec2.internal>
2023-09-16 00:43:21 +0000

    Initial commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  
+  config.hosts << "299e74be563740aab2af6b940d1e4056.vfs.cloud9.us-east-1.amazonaws.com"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
